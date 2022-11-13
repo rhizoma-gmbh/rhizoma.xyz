fbf0a7dde0e30ee4c4a250de578a5c0915a8fc57
Sebastian Martinez <me@sebastinez.dev>
2022-11-13 15:07:53 +0100

    Improve contact data
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,22 +3,34 @@
 </svelte:head>
 
 <h1 class="text-2xl mb-8">Lets keep in touch!</h1>
-
-<div class="grid grid-cols-4">
-  <div class="">Mail</div>
-  <div>
-    <a href="mailto:contact@rhizoma.xyz?subject=Contact from rhizoma.xyz">
-      contact@rhizoma.xyz
-    </a>
-  </div>
-</div>
-<div class="grid grid-cols-4">
-  <div>Twitter</div>
-  <div>@rhizoma</div>
-</div>
-<div class="grid grid-cols-4">
-  <div>ENS</div>
-  <div>
-    <a href="https://rhizoma.eth.xyz/">rhizoma.eth.xyz</a>
-  </div>
-</div>
+<table class="table-fixed leading-8">
+  <tr>
+    <td class="w-32">Mail</td>
+    <td>
+      <a
+        href="mailto:contact@rhizoma.xyz?subject=Contact from rhizoma.xyz"
+        class="hover:underline"
+      >
+        contact@rhizoma.xyz
+      </a>
+    </td>
+  </tr>
+  <tr>
+    <td> Twitter </td>
+    <td>
+      <a
+        href="https://twitter.com/rhizoma"
+        class="hover:underline hover:transition-all">@rhizoma</a
+      >
+    </td>
+  </tr>
+  <tr>
+    <td> ENS </td>
+    <td>
+      <a
+        href="https://rhizoma.eth.xyz/"
+        class="hover:underline hover:transition-all">rhizoma.eth.xyz</a
+      >
+    </td>
+  </tr>
+</table>
