1c7a6634fd50a8e1b08825194ccd1ef8937a96b4
Sebastian Martinez <me@sebastinez.dev>
2022-11-13 14:15:56 +0100

    Change the welcome page text
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
+<style>
+  p {
+    margin-bottom: 0.5rem;
+    text-align: justify;
+  }
+</style>
+
 <h1 class="text-xl mb-4" style:font-size="2rem" style:margin-bottom="2rem">
-  Bienvenidos
+  Welcome!
 </h1>
 <p>
-  Welcome to our web3 development studio! We specialize in building
-  decentralized applications and tools using the latest web3 technologies.
+  We specialize in building decentralized applications and tools using the
+  latest web3 technologies.
 </p>
 <p>
-  Our experienced team of developers can help you create dapps that are secure,
+  Our experienced developers can help you create dapps that are secure,
   efficient, and user-friendly. We are committed to helping you launch your dapp
-  on the Ethereum network and making it available to users worldwide.
+  and making it available to users worldwide.
 </p>
-<br />
 <p>Contact us today to get started!</p>
